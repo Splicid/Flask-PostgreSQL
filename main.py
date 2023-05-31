bd106d4fc25a4ab10eb5d6e8aea026f1c8421996
Splicid <luis1abreu11@gmail.com>
2023-05-30 22:02:28 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+import psycopg2
+from flask import Flask, render_template
+
+conn = psycopg2.connect(
+        host="localhost",
+        database="UserInfo",
+        user='postgres',
+        password='cirilo123'
+)
+
+# Open a cursor to perform database operations
+cur = conn.cursor()
+
+cur.execute('SELECT * FROM accounts')
+data = cur.fetchall()
+print(data)
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    cur.execute('SELECT * FROM accounts')
+    books = cur.fetchall()
+    return render_template('index.html', books=books)

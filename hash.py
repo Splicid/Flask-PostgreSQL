6b6eea28a1981ed8be0d9fdd63848885266a3637
Splicid <luis1abreu11@gmail.com>
2023-06-02 19:28:01 -0400

    Changed username and password to env variables

--- hash.py
+++ hash.py
@@ -1,6 +1,10 @@
+import os, pprint
 from passlib.hash import pbkdf2_sha256
 
+env_var = os.environ
+#pprint.pprint(dict(env_var), width = 1)
 
+#print(dict(env_var))
 def pass_encryption():
     password = pbkdf2_sha256.hash()
     print(pbkdf2_sha256.verify("passwo2rd", password))
--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from flask import Flask, render_template
 conn = psycopg2.connect(
         host="localhost",
         database="UserInfo",
-        user='postgres',
-        password='cirilo123'
+        user=os.environ['DB_USERNAME'],
+        password=os.environ['DB_PASSWORD']
 )
 
 # Open a cursor to perform database operations
